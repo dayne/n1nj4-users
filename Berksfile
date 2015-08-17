76756fea952d45e38f62020bd4068a04dcdbef00
Dayne Broderson <dayne@alaska.edu>
2015-08-16 22:49:17 -0800

    working as of this version

--- Berksfile
+++ Berksfile
@@ -2,7 +2,7 @@ source 'https://supermarket.chef.io'
 
 metadata
 
-cookbook 'chef-sugar', '~> 3.1.0'
+cookbook 'chef-sugar', git: 'https://github.com/sethvargo/chef-sugar.git'
 cookbook 'users', '~> 1.8.2'
 cookbook 'user', '~> 0.4.2'
 cookbook 'sudo', '~> 2.7.1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,14 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+include_recipe 'chef-sugar::default'
+
+#if raspbian? 
+  chef_gem 'ruby-shadow' do
+    action :install
+  end
+#end
+
 include_recipe "users"
 
 node.default['authorization']['sudo']['passwordless'] = true
